5d98a0d638b4c947901d873728b42b0ace36470e
eved1018 <edelsteinevan@gmail.com>
2025-02-27 22:21:41 -0500

    Sketch out game entities

--- clueless/src/main/java/cluelesscoders/clueless/Clueless.java
+++ clueless/src/main/java/cluelesscoders/clueless/Clueless.java
@@ -3,7 +3,9 @@
  */
 
 package cluelesscoders.clueless;
-
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
 /** To run, in command line use java Clueless c for client side
  *                              java Clueless s for server side
  *  Type ESC_SEQ in command line to terminate both programs
@@ -11,8 +13,67 @@ package cluelesscoders.clueless;
  * @author  Chris Dixon
  * @version 1.0
  */
-import java.io.*;
 public class Clueless {
+
+    enum Room {
+        ROOM1, ROOM2
+        
+    }
+
+    enum PlayerName {
+        player1, player2
+    }
+
+    enum Weapon {
+        knife, wrench
+    }
+
+    enum Card {
+        Room, Weapon, PlayerName
+    }
+
+    class Player {
+        ArrayList<Card>  hand;
+        PlayerName name;
+    }
+
+
+    class Turn implements Serializable {
+        PlayerName currPlayer;
+        Room currRoom;
+        
+        public Turn(PlayerName name, Room room){
+            this.currPlayer = name;
+            this.currRoom = room;
+        }
+    }
+
+    class Move extends Turn {
+        Room destination;
+
+        public Move(PlayerName name, Room room, Room dest) {
+            super(name, room);
+            this.destination = dest;
+        }
+    }
+
+    // suggestion and accusstion are the 
+    class Suggestion extends Turn {
+        PlayerName suspect;
+        Weapon murder_weapon;
+        boolean is_accusstion;
+        boolean was_moved_by_suggest;
+        // room is currRoom
+
+        public Suggestion(PlayerName name, Room room, PlayerName suspect, Weapon weapon, boolean is_accusstion) {
+            super(name, room);
+            this.suspect = suspect;
+            this.murder_weapon = weapon;
+            this.is_accusstion = is_accusstion;
+        }
+
+
+    }
 
     public static void main(String[] args) throws IOException{
         if(args[0].equals("s")){ 
@@ -22,9 +83,7 @@ public class Clueless {
         else if(args[0].equals("c")){
             Client c = new Client();
             c.start();
-        }
-        
-        
+        } 
     }
 }
 
